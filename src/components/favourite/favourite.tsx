73070519e5c5d08e92bddaf0a21157188b617b33
DeepthiNayak20 <deepthi.nayak@robosoftin.com>
2022-11-24 19:27:50 +0530

    last

--- src/components/favourite/favourite.tsx
+++ src/components/favourite/favourite.tsx
@@ -96,6 +96,7 @@ const Favourite = () => {
                     className="searchMobileImg"
                     onClick={() => {
                       dispatch(showModal())
+                      navigate('/')
                     }}
                   />
                 </div>
